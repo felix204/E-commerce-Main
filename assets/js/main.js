2f29f4853984eab1ffe90fe1ec029e0553415314
Akin Sel Coskun <akn_coskun@outlook.com>
2022-04-07 08:20:26 +0300

    product page updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,26 @@
 (function ($) {
     "use strict";
 
-    $(document).ready(function($){
-        
+    $(document).ready(function ($) {
+
         // testimonial sliders
         $(".testimonial-sliders").owlCarousel({
             items: 1,
             loop: true,
             autoplay: true,
-            responsive:{
-                0:{
-                    items:1,
-                    nav:false
+            responsive: {
+                0: {
+                    items: 1,
+                    nav: false
                 },
-                600:{
-                    items:1,
-                    nav:false
+                600: {
+                    items: 1,
+                    nav: false
                 },
-                1000:{
-                    items:1,
-                    nav:false,
-                    loop:true
+                1000: {
+                    items: 1,
+                    nav: false,
+                    loop: true
                 }
             }
         });
@@ -33,21 +33,21 @@
             nav: true,
             dots: false,
             navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
-            responsive:{
-                0:{
-                    items:1,
-                    nav:false,
-                    loop:true
+            responsive: {
+                0: {
+                    items: 1,
+                    nav: false,
+                    loop: true
                 },
-                600:{
-                    items:1,
-                    nav:true,
-                    loop:true
+                600: {
+                    items: 1,
+                    nav: true,
+                    loop: true
                 },
-                1000:{
-                    items:1,
-                    nav:true,
-                    loop:true
+                1000: {
+                    items: 1,
+                    nav: true,
+                    loop: true
                 }
             }
         });
@@ -58,36 +58,36 @@
             loop: true,
             autoplay: true,
             margin: 30,
-            responsive:{
-                0:{
-                    items:1,
-                    nav:false
+            responsive: {
+                0: {
+                    items: 1,
+                    nav: false
                 },
-                600:{
-                    items:3,
-                    nav:false
+                600: {
+                    items: 3,
+                    nav: false
                 },
-                1000:{
-                    items:4,
-                    nav:false,
-                    loop:true
+                1000: {
+                    items: 4,
+                    nav: false,
+                    loop: true
                 }
             }
         });
 
         // count down
-        if($('.time-countdown').length){  
-            $('.time-countdown').each(function() {
-            var $this = $(this), finalDate = $(this).data('countdown');
-            $this.countdown(finalDate, function(event) {
-                var $this = $(this).html(event.strftime('' + '<div class="counter-column"><div class="inner"><span class="count">%D</span>Days</div></div> ' + '<div class="counter-column"><div class="inner"><span class="count">%H</span>Hours</div></div>  ' + '<div class="counter-column"><div class="inner"><span class="count">%M</span>Mins</div></div>  ' + '<div class="counter-column"><div class="inner"><span class="count">%S</span>Secs</div></div>'));
+        if ($('.time-countdown').length) {
+            $('.time-countdown').each(function () {
+                var $this = $(this), finalDate = $(this).data('countdown');
+                $this.countdown(finalDate, function (event) {
+                    var $this = $(this).html(event.strftime('' + '<div class="counter-column"><div class="inner"><span class="count">%D</span>Days</div></div> ' + '<div class="counter-column"><div class="inner"><span class="count">%H</span>Hours</div></div>  ' + '<div class="counter-column"><div class="inner"><span class="count">%M</span>Mins</div></div>  ' + '<div class="counter-column"><div class="inner"><span class="count">%S</span>Secs</div></div>'));
+                });
             });
-         });
         }
 
         // projects filters isotop
         $(".product-filters li").on('click', function () {
-            
+
             $(".product-filters li").removeClass("active");
             $(this).addClass("active");
 
@@ -96,9 +96,9 @@
             $(".product-lists").isotope({
                 filter: selector,
             });
-            
+
         });
-        
+
         // isotop inner
         $(".product-lists").isotope();
 
@@ -123,19 +123,19 @@
         });
 
         // homepage slides animations
-        $(".homepage-slider").on("translate.owl.carousel", function(){
-            $(".hero-text-tablecell .subtitle").removeClass("animated fadeInUp").css({'opacity': '0'});
-            $(".hero-text-tablecell h1").removeClass("animated fadeInUp").css({'opacity': '0', 'animation-delay' : '0.3s'});
-            $(".hero-btns").removeClass("animated fadeInUp").css({'opacity': '0', 'animation-delay' : '0.5s'});
+        $(".homepage-slider").on("translate.owl.carousel", function () {
+            $(".hero-text-tablecell .subtitle").removeClass("animated fadeInUp").css({ 'opacity': '0' });
+            $(".hero-text-tablecell h1").removeClass("animated fadeInUp").css({ 'opacity': '0', 'animation-delay': '0.3s' });
+            $(".hero-btns").removeClass("animated fadeInUp").css({ 'opacity': '0', 'animation-delay': '0.5s' });
         });
 
-        $(".homepage-slider").on("translated.owl.carousel", function(){
-            $(".hero-text-tablecell .subtitle").addClass("animated fadeInUp").css({'opacity': '0'});
-            $(".hero-text-tablecell h1").addClass("animated fadeInUp").css({'opacity': '0', 'animation-delay' : '0.3s'});
-            $(".hero-btns").addClass("animated fadeInUp").css({'opacity': '0', 'animation-delay' : '0.5s'});
+        $(".homepage-slider").on("translated.owl.carousel", function () {
+            $(".hero-text-tablecell .subtitle").addClass("animated fadeInUp").css({ 'opacity': '0' });
+            $(".hero-text-tablecell h1").addClass("animated fadeInUp").css({ 'opacity': '0', 'animation-delay': '0.3s' });
+            $(".hero-btns").addClass("animated fadeInUp").css({ 'opacity': '0', 'animation-delay': '0.5s' });
         });
 
-       
+
 
         // stikcy js
         $("#sticker").sticky({
@@ -147,22 +147,69 @@
             meanMenuContainer: '.mobile-menu',
             meanScreenWidth: "992"
         });
-        
-         // search form
-        $(".search-bar-icon").on("click", function(){
+
+        // search form
+        $(".search-bar-icon").on("click", function () {
             $(".search-area").addClass("search-active");
         });
 
-        $(".close-btn").on("click", function() {
+        $(".close-btn").on("click", function () {
             $(".search-area").removeClass("search-active");
         });
-    
+
     });
 
 
-    jQuery(window).on("load",function(){
+    jQuery(window).on("load", function () {
         jQuery(".loader").fadeOut(1000);
     });
 
 
 }(jQuery));
+
+
+
+
+
+// Products
+
+
+const getData = async () => {
+    const response = await fetch('https://fakestoreapi.com/products/');
+    const JsonData = await response.json();
+
+    JsonData.map((product) => {
+
+        var productContent = "";
+
+        JsonData.forEach(product => {
+            productContent +=
+                `
+                <div class="col-lg-3 text-center p-3 mt-5" >
+                    <div class="single-product-item">
+                        <div class="product-image">
+                            <img src="${product.image}" alt="asd">
+                        </div>
+                    </div>
+                    <h3 class="lead">${product.title.substring(0,10)}</h3>
+                    <p class="product-price">$${product.price}</p>
+                    <a href="single-product.html" class="cart-btn"><i class="fas fa-shopping-cart"></i> Ürüne Git</a>
+                </div>
+            `;
+
+        });
+
+        var results = document.querySelector('.product-lists');
+
+        results.innerHTML = productContent;
+
+    });
+}
+
+
+getData();
+
+
+
+
+
